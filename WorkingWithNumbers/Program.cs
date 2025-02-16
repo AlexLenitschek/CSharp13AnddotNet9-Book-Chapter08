e37aedbdfa3ca4c098407acbe3f1a54544566fd8
alex lenitschek <Alexander.lenitschek@gmx.de>
2025-02-16 19:19:44 +0100

    Add project files.

---
+++ WorkingWithNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics; // To use BigInteger.
+
+const int width = 40;
+
+WriteLine("ulong.MaxValue vs a 30-digit BigInteger");
+WriteLine(new string('-', width));
+
+ulong big = ulong.MaxValue;
+WriteLine($"{big,width:N0}");
+
+BigInteger bigger = BigInteger.Parse("123456789012345678901234567890");
+WriteLine($"{bigger,width:N0}");
