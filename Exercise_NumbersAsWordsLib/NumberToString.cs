97d7d1762778d4083fc232c1fa93518e58208363
AlexLenitschek <alexander.lenitschek@gmx.de>
2025-02-22 00:09:08 +0100

    adding Exercise 8.2 Extension methods. TODO: Finish lib, add xunit test and console test

---
+++ Exercise_NumbersAsWordsLib/NumberToString.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace Packt.Shared
+{
+    public static class NumberToString
+    {
+        public static string ToWords(this int input)
+        {
+            return ToWords((BigInteger)input);
+        }
+
+        public static string ToWords(this long input)
+        {
+            return ToWords((BigInteger)input);
+        }
+
+        public static string ToWords(this BigInteger input)
+        {
+            return "Not Implemented Yet."; // Placeholder. All the translation logic will be implemented in this method.
+        }
+    }
+}
